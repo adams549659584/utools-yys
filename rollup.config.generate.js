0d4a0df36226fcbee4090ffc885c282e36ca433e
罗君 <13760614423@163.com>
2020-08-17 09:56:57 +0800

    sourcemap

--- rollup.config.generate.js
+++ rollup.config.generate.js
@@ -5,7 +5,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 // import copy from 'rollup-plugin-copy';
 import replace from 'rollup-plugin-replace';
-import { terser } from "rollup-plugin-terser";
+import { terser } from 'rollup-plugin-terser';
 
 /**
  * 当前环境
@@ -20,7 +20,7 @@ const rollupOptions = {
   output: {
     file: './dist/generate.js',
     format: 'cjs',
-    sourcemap: NODE_ENV === 'production' ? 'hidden' : 'inline',
+    sourcemap: NODE_ENV === 'production' ? false : 'inline',
   },
   plugins: [
     // cleaner({
